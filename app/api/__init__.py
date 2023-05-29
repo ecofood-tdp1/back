8d93e28d3f8ea97e2f06c6c837abef4cc0f7130d
Tomas Macia <tomasmacia@gmail.com>
2023-05-29 00:23:11 -0300

    Orders

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 
+from .orders import orders_router
 from .packs import packs_router
 from .shops import shops_router
 from .transactions import transactions_router
@@ -10,3 +11,4 @@ api_router = APIRouter()
 api_router.include_router(shops_router, tags=["Shops"], prefix="/shops")
 api_router.include_router(packs_router, tags=["Packs"], prefix="/packs")
 api_router.include_router(transactions_router, tags=["Transactions"], prefix="/transactions")
+api_router.include_router(orders_router, tags=["Orders"], prefix="/orders")
