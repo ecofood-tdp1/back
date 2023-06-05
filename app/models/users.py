a78983c8d47a3c7534b24cacc9dde849bb34efd2
Agustin Gabrielli <agabrielli@fi.uba.ar>
2023-06-04 22:02:54 -0300

    Added type and display name to users

--- app/models/users.py
+++ app/models/users.py
@@ -1,15 +1,24 @@
 import uuid
-
+from enum import Enum
 from pydantic import BaseModel, Field
 from typing import List, Optional
 
+class UserType(Enum):
+    buyer = "buyer"
+    shop = "shop"
+
+
 class User(BaseModel):
     id: str = Field(default_factory=uuid.uuid4, alias="_id")
+    type: UserType = Field(...)
+    display_name: str = Field(...)
     pack_ids: Optional[List[str]] = Field(...)
     
     class Config:
         schema_extra = {
             "example": {
+                "type": "buyer",
+                "display_name": "Lionel Messi",
                 "pack_ids": [
                     "89128j1982d12"
                 ]
